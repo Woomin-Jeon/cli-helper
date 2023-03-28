951e0eae68d86171e72e87644e02dbf01fa0f206
Woomin-Jeon <dal96k@gmail.com>
2023-03-28 21:14:30 +0900

    init

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    'no-console': 'off',
+    'import/prefer-default-export': 'off',
+    'max-len': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/extensions': 'off',
+  },
+};
