31ad7781c3cca0ebf17748d86eb2c9f7d21776db
Woomin-Jeon <dal96k@gmail.com>
2023-05-22 16:43:32 +0900

    fix: reset HEAD 하기 전에 확인하는 프롬프트 추가

--- src/commands/g/rh/index.ts
+++ src/commands/g/rh/index.ts
@@ -5,6 +5,14 @@ export default class GitResetHead extends Command {
   async run() {
     console.clear();
     const count = await ux.prompt('How many commits need to be reset');
+    const answer = await ux.prompt(`Execute "git reset HEAD~${count}". Are you sure? yes/no`, {
+      required: true,
+      default: 'yes',
+    });
+
+    if (answer !== 'yes') {
+      return;
+    }
 
     exec(`git reset HEAD~${count}`);
     this.exit();
